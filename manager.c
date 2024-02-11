1db55fe76e97ce852cff470039eec7c3ad5f9057
aarora80 <aarora80@asu.edu>
2024-02-11 16:49:21 -0700

    Initial commit

---
+++ manager.c
@@ -0,0 +1,177 @@
+// Implements the server side of an echo client-server application program.
+// The client reads ITERATIONS strings from stdin, passes the string to the
+// this server, which simply sends the string back to the client.
+//
+// Compile on general.asu.edu as:
+//   g++ -o server UDPEchoServer.c
+//
+// Only on general3 and general4 have the ports >= 1024 been opened for
+// application programs.
+#include <stdio.h>      // for printf() and fprintf()
+#include <sys/socket.h> // for socket() and bind()
+#include <arpa/inet.h>  // for sockaddr_in and inet_ntoa()
+#include <stdlib.h>     // for atoi() and exit()
+#include <string.h>     // for memset()
+#include <unistd.h>     // for close()
+
+#define ECHOMAX 255     // Longest string to echo
+#define MAX_PEERS 10 // Maximum number of peers that can be registered
+#define MAX_NAME_LENGTH 15 // Maximum length of peer names
+#define MAX_MSG_LENGTH 255 // Maximum length of messages
+#define MANAGER_PORT 5000 // Port number for manager
+
+struct Peer {
+    char name[MAX_NAME_LENGTH + 1];
+    char ip_address[16]; // IPv4 address format "xxx.xxx.xxx.xxx\0"
+    unsigned int m_port; // Port for communication between peer and manager
+    unsigned int p_port; // Port for communication between peers
+    char state[10]; // State of the peer: Free, Leader, InDHT
+};
+
+struct Peer registered_peers[MAX_PEERS]; // Array to store registered peers
+int num_registered_peers = 0; // Counter for registered peers
+
+void DieWithError( const char *errorMessage ) // External error handling function
+{
+    perror( errorMessage );
+    exit( 1 );
+}
+
+//register Peer
+int register_peer(char *peer_name, char *ip_address, unsigned int m_port, unsigned int p_port, struct sockaddr_in echoClntAddr, int sock) {
+    // The function now directly accepts peer information instead of the message
+    // No need to parse the message, as the information is provided directly
+
+    // Debugging: Print parsed values
+    printf("Parsed peer name: %s\n", peer_name);
+    printf("Parsed IP address: %s\n", ip_address);
+    printf("Parsed manager port: %u\n", m_port);
+    printf("Parsed peer port: %u\n", p_port);
+
+    // Check if peer name is already registered
+    for (int i = 0; i < num_registered_peers; i++) {
+        if (strcmp(registered_peers[i].name, peer_name) == 0) {
+            // Send failure response if peer name is already registered
+            sendto(sock, "FAILURE: Peer name already registered", strlen("FAILURE: Peer name already registered"), 0,
+                   (struct sockaddr *) &echoClntAddr, sizeof(echoClntAddr));
+            return 0;
+        }
+    }
+
+    // Check if ports are already in use by other peers
+    for (int i = 0; i < num_registered_peers; i++) {
+        if (strcmp(registered_peers[i].ip_address, ip_address) == 0 && 
+            (registered_peers[i].m_port == m_port || registered_peers[i].p_port == p_port)) {
+            // Send failure response if ports are not unique
+            sendto(sock, "FAILURE: Duplicate IP address and ports combination", 
+                   strlen("FAILURE: Duplicate IP address and ports combination"), 0,
+                   (struct sockaddr *) &echoClntAddr, sizeof(echoClntAddr));
+            return 0;
+        }
+    }
+
+    // Register the new peer
+    strcpy(registered_peers[num_registered_peers].name, peer_name);
+    strcpy(registered_peers[num_registered_peers].ip_address, ip_address);
+    registered_peers[num_registered_peers].m_port = m_port;
+    registered_peers[num_registered_peers].p_port = p_port;
+    strcpy(registered_peers[num_registered_peers].state, "Free");
+
+    // Increment the counter for registered peers
+    num_registered_peers++;
+
+    // Send success response to the peer
+    sendto(sock, "SUCCESS!", strlen("SUCCESS"), 0, (struct sockaddr *) &echoClntAddr, sizeof(echoClntAddr));
+    return 1;
+}
+
+void print_registered_peers() {
+    printf("Registered Peers:\n");
+    for (int i = 0; i < num_registered_peers; i++) {
+        printf("Peer %d:\n", i);
+        printf("Name: %s\n", registered_peers[i].name);
+        printf("IP Address: %s\n", registered_peers[i].ip_address);
+        printf("Manager Port: %d\n", registered_peers[i].m_port);
+        printf("Peer Port: %d\n", registered_peers[i].p_port);
+        printf("State: %s\n", registered_peers[i].state);
+        printf("\n");
+    }
+}
+
+int main( int argc, char *argv[] )
+{
+    int sock;                        // Socket
+    struct sockaddr_in echoServAddr; // Local address of server
+    struct sockaddr_in echoClntAddr; // Client address
+    unsigned int cliAddrLen;         // Length of incoming message
+    char echoBuffer[ ECHOMAX ];      // Buffer for echo string
+    unsigned short echoServPort;     // Server port
+    int recvMsgSize;                 // Size of received message
+
+    if( argc != 2 )         // Test for correct number of parameters
+    {
+        fprintf( stderr, "Usage:  %s <UDP SERVER PORT>\n", argv[ 0 ] );
+        exit( 1 );
+    }
+
+    echoServPort = atoi(argv[1]);  // First arg: local port
+
+    // Create socket for sending/receiving datagrams
+    if( ( sock = socket( PF_INET, SOCK_DGRAM, IPPROTO_UDP ) ) < 0 )
+        DieWithError( "server: socket() failed" );
+
+    // Construct local address structure */
+    memset( &echoServAddr, 0, sizeof( echoServAddr ) ); // Zero out structure
+    echoServAddr.sin_family = AF_INET;                  // Internet address family
+    echoServAddr.sin_addr.s_addr = htonl( INADDR_ANY ); // Any incoming interface
+    echoServAddr.sin_port = htons( echoServPort );      // Local port
+
+    // Bind to the local address
+    if( bind( sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0 )
+        DieWithError( "server: bind() failed" );
+
+	printf( "server: Port server is listening to is: %d\n", echoServPort );
+
+    for (;;) { // Run forever
+    cliAddrLen = sizeof(echoClntAddr);
+
+    // Block until receive message from a client
+    if ((recvMsgSize = recvfrom(sock, echoBuffer, ECHOMAX, 0, (struct sockaddr *)&echoClntAddr, &cliAddrLen)) < 0)
+        DieWithError("server: recvfrom() failed");
+
+    echoBuffer[recvMsgSize] = '\0';
+
+    //printf("server: received string ``%s'' from client on IP address %s\n", echoBuffer, inet_ntoa(echoClntAddr.sin_addr));
+
+    // Extract and print peer information
+    char command[10];
+    char peer_name[MAX_NAME_LENGTH + 1];
+    char ip_address[16];
+    unsigned int m_port, p_port;
+    sscanf(echoBuffer, "%9s %s %s %u %u", command, peer_name, ip_address, &m_port, &p_port);
+
+    // Check message type and process accordingly
+    if (strcmp(command, "register") == 0) {
+        printf("Peer information received:\n");
+        printf("Command: %s\n", command);
+        printf("Name: %s\n", peer_name);
+        printf("IP Address: %s\n", ip_address);
+        printf("Manager Port: %u\n", m_port);
+        printf("Peer Port: %u\n", p_port);
+        if (register_peer(peer_name, ip_address, m_port, p_port, echoClntAddr, sock)) {
+            printf("Peer registered successfully.\n");
+            print_registered_peers();
+        } else {
+            printf("Failed to register peer.\n");
+        }
+    } else {
+        // Handle other types of messages here
+        // For now, just echo back the message to the client
+        if (sendto(sock, echoBuffer, recvMsgSize, 0, (struct sockaddr *)&echoClntAddr,
+                   sizeof(echoClntAddr)) != recvMsgSize)
+            DieWithError("server: sendto() sent a different number of bytes than expected");
+    }
+}
+
+    // NOT REACHED */
+}
---
+++ peer.c
@@ -0,0 +1,183 @@
+// Implements the client side of an echo client-server application program.
+// The client reads ITERATIONS strings from stdin, passes the string to the
+// server, which simply echoes it back to the client.
+//
+// Compile on general.asu.edu as:
+//   g++ -o client UDPEchoClient.c
+//
+// Only on general3 and general4 have the ports >= 1024 been opened for
+// application programs.
+#include <stdio.h>      // for printf() and fprintf()
+#include <sys/socket.h> // for socket(), connect(), sendto(), and recvfrom()
+#include <arpa/inet.h>  // for sockaddr_in and inet_addr()
+#include <stdlib.h>     // for atoi() and exit()
+#include <string.h>     // for memset()
+#include <unistd.h>     // for close()
+#include <string.h>
+
+#define ECHOMAX 255     // Longest string to echo
+#define ITERATIONS	5   // Number of iterations the client executes
+#define ECHOMAX 255     // Longest string to echo
+#define MAX_PEERS 10 // Maximum number of peers that can be registered
+#define MAX_NAME_LENGTH 15 // Maximum length of peer names
+#define MAX_MSG_LENGTH 255 // Maximum length of messages
+#define MANAGER_PORT 5000 // Port number for manager
+#define MAX_COMMAND_LENGTH 10
+#define MAX_MESSAGE_SIZE 256
+
+
+struct Peer {
+    char command[MAX_COMMAND_LENGTH + 1]; // Command string
+    char name[MAX_NAME_LENGTH + 1];
+    char ip_address[16]; // IPv4 address format "xxx.xxx.xxx.xxx\0"
+    unsigned int m_port; // Port for communication between peer and manager
+    unsigned int p_port; // Port for communication between peers
+    char state[10]; // State of the peer: Free, Leader, InDHT
+};
+
+void DieWithError( const char *errorMessage ) // External error handling function
+{
+    perror( errorMessage );
+    exit(1);
+}
+
+int main( int argc, char *argv[] )
+{
+    size_t nread;
+    int sock;                        // Socket descriptor
+    struct sockaddr_in echoServAddr; // Echo server address
+    struct sockaddr_in fromAddr;     // Source address of echo
+    unsigned short echoServPort;     // Echo server port
+    unsigned int fromSize;           // In-out of address size for recvfrom()
+    char *servIP;                    // IP address of server
+    // char *echoString = NULL;         // String to send to echo server
+    size_t echoStringLen = ECHOMAX;               // Length of string to echo
+    int respStringLen;               // Length of received response
+    char echoBuffer[ECHOMAX];
+    //char message[MAX_MESSAGE_SIZE];
+
+    // echoString = (char *) malloc( ECHOMAX );
+
+    if (argc < 3)    // Test for correct number of arguments
+    {
+        fprintf( stderr, "Usage: %s <Server IP address> <Echo Port>\n", argv[0] );
+        exit( 1 );
+    }
+
+
+    servIP = argv[ 1 ];  // First arg: server IP address (dotted decimal)
+    echoServPort = atoi( argv[2] );  // Second arg: Use given port
+
+    
+    printf( "client: Arguments passed: server IP %s, port %d\n", servIP, echoServPort );
+    
+    //printf("CREATING the socket");
+    // Create a datagram/UDP socket
+    if( ( sock = socket( PF_INET, SOCK_DGRAM, IPPROTO_UDP ) ) < 0 )
+        DieWithError( "client: socket() failed" );
+
+    //printf("I have created the socket");
+    // Construct the server address structure
+    memset( &echoServAddr, 0, sizeof( echoServAddr ) ); // Zero out structure
+    echoServAddr.sin_family = AF_INET;                  // Use internet addr family
+    echoServAddr.sin_addr.s_addr = inet_addr( servIP ); // Set server's IP address
+    echoServAddr.sin_port = htons( echoServPort );      // Set server's port
+
+    //printf("I have made it before the while loop");
+  
+    while(1){
+    //printf("I have made it into the while loop");
+
+    printf("Enter a command: ");
+        char command[10];
+        if (scanf("%9s", command) != 1) {
+            fprintf(stderr, "Error reading command.\n");
+            break; // Exit the loop if there's an error
+        }
+    // Check if the command is "register"
+    if (strcmp(command, "register") == 0) {
+        //printf("I have made it into register");
+        struct Peer peer;
+        // If it's a registration command
+        strcpy(peer.command, "register");
+        printf("Enter peer name: ");
+            scanf("%49s", peer.name);
+            printf("Enter peer IP address: ");
+            scanf("%15s", peer.ip_address);
+            printf("Enter manager port: ");
+            scanf("%d", &peer.m_port);
+            printf("Enter peer port: ");
+            scanf("%d", &peer.p_port);
+
+            // Print the registration information
+            printf("Registered:\n");
+            printf("Peer: %s\n", peer.name);
+            printf("IP Address: %s\n", peer.ip_address);
+            printf("Manager Port: %d\n", peer.m_port);
+            printf("Peer Port: %d\n", peer.p_port);
+
+            char message[MAX_MESSAGE_SIZE];
+            sprintf(message, "register %s %s %d %d", peer.name, peer.ip_address, peer.m_port, peer.p_port);
+
+            // Send the peer information to the server
+            if (sendto(sock, message, strlen(message), 0, (struct sockaddr *)&echoServAddr, sizeof(echoServAddr)) < 0) {
+                perror("sendto failed");
+                exit(1);
+        }
+
+        
+    }else if (strcmp(command, "exit") == 0) {
+        // If the command is "exit"
+        exit(0);
+    } else {
+        // If the command is invalid
+        printf("Invalid command.\n");
+    }
+
+    // Send the commands to the server
+    // if (sendto(sock, echoBuffer, echoStringLen, 0, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) != echoStringLen)
+    //     DieWithError("client: sendto() sent a different number of bytes than expected");
+
+    // Receive a response
+    if (recvfrom(sock, echoBuffer, ECHOMAX, 0, NULL, NULL) < 0)
+        DieWithError("client: recvfrom() failed");
+
+    printf("Received response from server: %s\n", echoBuffer);
+
+    }
+	// Pass string back and forth between server ITERATIONS times
+
+	// printf( "client: Echoing strings for %d iterations\n", ITERATIONS );
+
+    // for( int i = 0; i < ITERATIONS; i++ )
+    // {
+    //     printf( "\nEnter string to echo: \n" );
+    //     if( ( nread = getline( &echoString, &echoStringLen, stdin ) ) != -1 )
+    //     {
+    //         echoString[ (int) strlen( echoString) - 1 ] = '\0'; // Overwrite newline
+    //         printf( "\nclient: reads string ``%s''\n", echoString );
+    //     }
+    //     else
+    //         DieWithError( "client: error reading string to echo\n" );
+
+    //     // Send the string to the server
+    //     if( sendto( sock, echoString, strlen( echoString ), 0, (struct sockaddr *) &echoServAddr, sizeof( echoServAddr ) ) != strlen(echoString) )
+    //    		DieWithError( "client: sendto() sent a different number of bytes than expected" );
+
+    //     // Receive a response
+    //     fromSize = sizeof( fromAddr );
+
+    //     if( ( respStringLen = recvfrom( sock, echoString, ECHOMAX, 0, (struct sockaddr *) &fromAddr, &fromSize ) ) > ECHOMAX )
+    //         DieWithError( "client: recvfrom() failed" );
+
+    //     echoString[ respStringLen ] = '\0';
+
+    //     if( echoServAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr )
+    //         DieWithError( "client: Error: received a packet from unknown source.\n" );
+
+ 	// 	printf( "client: received string ``%s'' from server on IP address %s\n", echoString, inet_ntoa( fromAddr.sin_addr ) );
+    // }
+    
+    close( sock );
+    return 0;
+}
